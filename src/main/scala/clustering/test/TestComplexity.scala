e43918f4d2167386daffa779eab6658fa6e58166
Daniel <dtizon@informa.es>
2017-01-18 15:32:14 +0100

    * Validaciones RandIndex

--- src/main/scala/clustering/test/TestComplexity.scala
+++ src/main/scala/clustering/test/TestComplexity.scala
@@ -12,6 +12,7 @@ import org.apache.spark.ml.clustering.BisectingKMeans
 import org.apache.spark.ml.clustering.GaussianMixture
 import org.apache.spark.mllib.clustering.PowerIterationClustering
 import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.functions.rand
 
 object TestComplexity {
 
@@ -28,7 +29,7 @@ object TestComplexity {
       .withColumn("SepalWidth", col("SepalWidth").cast("Double"))
       .withColumn("PetalLength", col("PetalLength").cast("Double"))
       .withColumn("PetalWidth", col("PetalWidth").cast("Double"))
-      .withColumnRenamed("index", "id")//.sample(true, 1000.0)
+      .withColumnRenamed("index", "id")//.sample(true, 10.0)
 
     val vectorData = new VectorAssembler().setInputCols(Array("SepalLength", "SepalWidth", "PetalLength", "PetalWidth")).setOutputCol("features")
       .transform(ds).select("id", "features").cache
@@ -40,64 +41,59 @@ object TestComplexity {
     val maxIterations = 20
     val k = 3
 
-//    val tIni = new Date().getTime
-//    val res1 = new KMeans().setK(k).setMaxIter(maxIterations).fit(vectorData).transform(vectorData)
-//    res1.count
-//    val tFin = new Date().getTime
-//
-//    val tEmpleado = (tFin - tIni) / 1000.0
-//
-//    val tIni2 = new Date().getTime
-//    val res2 = new BisectingKMeans().setK(k).setMaxIter(maxIterations).fit(vectorData).transform(vectorData)
-//    res2.count
-//    val tFin2 = new Date().getTime
-//
-//    val tEmpleado2 = (tFin2 - tIni2) / 1000.0
-
-    val tIni3 = new Date().getTime
-    val model = new GaussianMixture().setK(k).setMaxIter(maxIterations).fit(vectorData)
-    model.gaussians.foreach(x => {
-      println("MEDIA: "+x.mean)
-      println("COVARIANZA: "+x.cov.toString())
-      
-    })
-    val weights = model.weights
-    println("WEIGHTS: "+weights.mkString(","))
-    val res3 = model.transform(vectorData)    
-    res3.count
-    val tFin3 = new Date().getTime
-
-    val tEmpleado3 = (tFin3 - tIni3) / 1000.0
-
-    //    val indexedDataset = ds.drop("Specie", "id").rdd.zipWithIndex().cache
-    //    indexedDataset.count
-    //    val tIni4 = new Date().getTime
+    //    val tIni = new Date().getTime
+    //    val res1 = new KMeans().setK(k).setMaxIter(maxIterations).fit(vectorData).transform(vectorData)
+    //    res1.count
+    //    val tFin = new Date().getTime
     //
-    //    val distancesRdd = indexedDataset.cartesian(indexedDataset).flatMap { x =>
-    //      val index1 = x._1._2
-    //      val index2 = x._2._2
-    //      if (index1 < index2) {
-    //        val vectorValues1 = Vectors.dense(x._1._1.toSeq.toArray.map(x => x.toString().toDouble))
-    //        val vectorValues2 = Vectors.dense(x._2._1.toSeq.toArray.map(x => x.toString().toDouble))
-    //        val distance = sim(vectorValues1, vectorValues2)
-    //        Some(index1, index2, distance)
-    //      } else {
-    //        None
-    //      }
-    //    }
+    //    val tEmpleado = (tFin - tIni) / 1000.0
     //
-    //    val model = new PowerIterationClustering().setK(k).setMaxIterations(maxIterations).setInitializationMode("degree").run(distancesRdd)
-    //    val clusters = model.assignments.collect().groupBy(_.cluster).mapValues(_.map(_.id)).toList
-    //    clusters.toList.foreach(x => println(x._1 + ": " + x._2.size))
+    //    val tIni2 = new Date().getTime
+    //    val res2 = new BisectingKMeans().setK(k).setMaxIter(maxIterations).fit(vectorData).transform(vectorData)
+    //    res2.count
+    //    val tFin2 = new Date().getTime
     //
-    //    val tFin4 = new Date().getTime
-    //
-    //    val tEmpleado4 = (tFin4 - tIni4) / 1000.0
+    //    val tEmpleado2 = (tFin2 - tIni2) / 1000.0
 
-//    println(f"El proceso ha finalizado con KMeans ha sido $tEmpleado%2.2f segundos")
-//    println(f"El proceso ha finalizado con BisectingKMeans ha sido $tEmpleado2%2.2f segundos")
-    println(f"El proceso ha finalizado con GaussianMixgures ha sido $tEmpleado3%2.2f segundos")
-    //    println(f"El proceso ha finalizado con PowerIterationClustering ha sido $tEmpleado4%2.2f segundos")
+    //    val tIni3 = new Date().getTime
+    //    val model = new GaussianMixture().setK(k).setMaxIter(maxIterations).fit(vectorData)
+    //    val res3 = model.transform(vectorData)    
+    //    res3.count
+    //    val tFin3 = new Date().getTime
+    //
+    //    val tEmpleado3 = (tFin3 - tIni3) / 1000.0
+
+    val n = 100
+    val indexedDataset = Spark.spark.sqlContext.range(n).withColumn("feature1", rand).withColumn("feature2", rand).withColumn("feature3", rand).withColumn("feature4", rand).distinct.rdd
+    indexedDataset.count
+    val tIni4 = new Date().getTime
+
+    val distancesRdd = indexedDataset.cartesian(indexedDataset).flatMap { x =>
+      val index1 = x._1.getAs[Long]("id")
+      val index2 = x._2.getAs[Long]("id")
+      if (index1 < index2) {
+        val vectorValues1 = Vectors.dense(x._1.toSeq.tail.toArray.map(x => x.toString().toDouble))
+        val vectorValues2 = Vectors.dense(x._2.toSeq.tail.toArray.map(x => x.toString().toDouble))
+        val distance = sim(vectorValues1, vectorValues2)
+        Some(index1, index2, distance)
+      } else {
+        None
+      }
+    }
+
+    println("distancesRdd count: "+distancesRdd.count)
+    val model = new PowerIterationClustering().setK(k).setMaxIterations(maxIterations).setInitializationMode("degree").run(distancesRdd)
+    val clusters = model.assignments.collect().groupBy(_.cluster).mapValues(_.map(_.id)).toList
+    clusters.toList.foreach(x => println(x._1 + ": " + x._2.size))
+
+    val tFin4 = new Date().getTime
+
+    val tEmpleado4 = (tFin4 - tIni4) / 1000.0
+
+    //    println(f"El proceso ha finalizado con KMeans ha sido $tEmpleado%2.2f segundos")
+    //    println(f"El proceso ha finalizado con BisectingKMeans ha sido $tEmpleado2%2.2f segundos")
+    //    println(f"El proceso ha finalizado con GaussianMixgures ha sido $tEmpleado3%2.2f segundos")
+    println(f"El proceso ha finalizado con PowerIterationClustering ha sido $tEmpleado4%2.2f segundos")
 
     Spark.spark.stop()
   }
---
+++ src/main/scala/clustering/test/TestRandIndex2.scala
@@ -0,0 +1,61 @@
+package clustering.test
+
+import java.util.Date
+
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.functions.col
+
+import clustering.metrics.ClusteringIndexes
+import clustering.metrics.Spark
+import clustering.metrics.indexes.IndexRand
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.clustering.BisectingKMeans
+import org.apache.spark.ml.clustering.GaussianMixture
+import org.apache.spark.mllib.clustering.PowerIterationClustering
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.rdd.RDD
+import clustering.metrics.Utils.standarize
+
+object TestRandIndex2 {
+  def main(args: Array[String]) {
+
+    //    Spark.conf.setAppName("Clustering-metrics")
+    //      .set("spark.ui.port", "1982")
+    //      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    //      .set("spark.sql.warehouse.dir", "/user/dtizon/spark-warehouse")
+
+    Spark.conf.setAppName("Test Data Iris")
+      .setMaster("local[*]")
+      .set("spark.sql.warehouse.dir", "spark-warehouse")
+
+    val spark = Spark.spark
+
+    val dataset = spark.read.parquet("/home/tornar/TGAS-Exp1").withColumnRenamed("tycho2_id", "id")    
+    val vectorData = new VectorAssembler().setInputCols(Array("ra", "dec", "pmra", "pmdec", "parallax")).setOutputCol("features").transform(dataset).select("id", "features")
+
+    // Estandarizar datos
+    val scaledDS = standarize(vectorData).cache
+
+    val maxIterations = 20
+    val k = 20
+
+    val res = new KMeans().setK(k).setMaxIter(maxIterations).fit(scaledDS).transform(scaledDS)
+    //val res = new BisectingKMeans().setK(k).setMaxIter(maxIterations).fit(scaledDS).transform(scaledDS)
+    //val res = new GaussianMixture().setK(k).setMaxIter(maxIterations).fit(scaledDS).transform(scaledDS)
+
+    res.select("id", "prediction").join(dataset, "id").sample(false, 0.25).coalesce(1).write.option("header", true).csv("/home/tornar/TGAS_1_KMeans_20_CSV")
+    
+    val evidencia = spark.read.option("header", true).csv("/home/tornar/Dropbox/Inteligencia Artificial/TFM/ValidacionExterna.csv").rdd.map(x => (x.getAs[String]("CUMULO"), x.getAs[String]("TYCHO")))
+
+    val evidenciaAgrupados: RDD[Tuple3[Long, String, String]] = evidencia.groupByKey.flatMap(x => x._2.toSet.subsets(2)).map(x => (x.head, x.last)).zipWithIndex().map(x => (x._2, x._1._1, x._1._2))
+    val evidenciaSeparados: RDD[Tuple3[Long, String, String]] = evidencia.cartesian(evidencia).filter(x => x._1._1 != x._2._1).map(x => (x._1._2, x._2._2)).zipWithIndex().map(x => (x._2, x._1._1, x._1._2))
+
+    val tIni = new Date().getTime
+    val randIndex = IndexRand.calculate(res, evidenciaAgrupados, evidenciaSeparados)
+    val tFin = new Date().getTime
+    val tEmpleado = (tFin - tIni) / 1000.0
+    println("Rand Index: " + randIndex)
+    println("Tiempo empleado: " + tEmpleado)
+  }
+
+}
